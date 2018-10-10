e66d1775d177c61c07c125fa487fee5072f1aa5e
Aaron Frank <aaron.frank41@gmail.com>
2018-10-10 13:46:13 -0700

    created binary search project

---
+++ Challenges/binarySearch/binarySearch/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static BinarySearch.Program;
+
+namespace BinarySearchTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
