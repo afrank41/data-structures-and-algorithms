8205f0fa9e3c1c4fb7a215c5766522a30b9bba56
Aaron Frank <aaron.frank41@gmail.com>
2018-10-10 13:58:06 -0700

    added tests and created binary search

--- Challenges/binarySearch/binarySearch/BinarySearch/Program.cs
+++ Challenges/binarySearch/binarySearch/BinarySearch/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace BinarySearch
+namespace ArrayBinarySearch
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int BinarySearch(int[] arr, int val)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int mid = (end - start) / 2;
+
+                if(val == arr[mid])
+                {
+                    return mid;
+                }
+                else if (val > arr[mid])
+                {
+                    start = mid + 1;
+                }
+                else if (val < arr[mid])
+                {
+                    end = mid - 1;
+                }
+            }
+            return -1;
         }
     }
 }
--- Challenges/binarySearch/binarySearch/XUnitTestProject1/UnitTest1.cs
+++ Challenges/binarySearch/binarySearch/XUnitTestProject1/UnitTest1.cs
@@ -1,15 +1,36 @@
 using System;
 using Xunit;
-using static BinarySearch.Program;
+using static ArrayBinarySearch.Program;
 
 namespace BinarySearchTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanReturnCorrectIndex()
         {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            int value = 3;
 
+            Assert.Equal(2, BinarySearch(array, value));
+        }
+
+        [Fact]
+        public void CannotReturnCorrectIndex()
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            int value = 6;
+
+            Assert.Equal(-1, BinarySearch(array, value));
+        }
+
+        [Fact]
+        public void CanReturnCorrectIndexIfArrayHasOnlyOne()
+        {
+            int[] array = new int[1] { 1 };
+            int value = 1;
+
+            Assert.Equal(0, BinarySearch(array, value));
         }
     }
 }
