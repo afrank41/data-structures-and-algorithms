62c16dce507b6d048328d1ee001181477d5a14c0
Aaron Frank <aaron.frank41@gmail.com>
2018-10-15 17:25:20 -0700

    added remaining tests

--- Data-Structures/linkedList/LinkedLists/LinkedListTest/UnitTest1.cs
+++ Data-Structures/linkedList/LinkedLists/LinkedListTest/UnitTest1.cs
@@ -44,5 +44,62 @@ namespace LinkedListTest
             Assert.Equal(value, list.Find(value).Value);
         }
 
+        [Theory]
+        [InlineData(20)]
+        [InlineData("bird")]
+        [InlineData(true)]
+        public void LLCanAddBefore(object value)
+        {
+            Node node1 = new Node(7);
+            Node node2 = new Node("cat");
+            Node node3 = new Node("dog");
+            Node node4 = new Node(value);
+
+            LinkedList list = new LinkedList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.AddBefore(node4, node3);
+
+            Assert.Equal(value, list.Find(value).Value);
+        }
+
+        [Theory]
+        [InlineData(20)]
+        [InlineData("bird")]
+        [InlineData(true)]
+        public void LLCanAddAfter(object value)
+        {
+            Node node1 = new Node(7);
+            Node node2 = new Node("cat");
+            Node node3 = new Node("dog");
+            Node node4 = new Node(value);
+
+            LinkedList list = new LinkedList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.AddBefore(node4, node2);
+
+            Assert.Equal(value, list.Find(value).Value);
+        }
+
+        [Theory]
+        [InlineData(20)]
+        [InlineData("bird")]
+        [InlineData(true)]
+        public void LLCanFind(object value)
+        {
+            Node node1 = new Node(7);
+            Node node2 = new Node("cat");
+            Node node3 = new Node("dog");
+            Node node4 = new Node(value);
+
+            LinkedList list = new LinkedList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.Append(node4);
+
+            Assert.Equal(value, list.Find(value).Value);
+        }
+
     }
 }
