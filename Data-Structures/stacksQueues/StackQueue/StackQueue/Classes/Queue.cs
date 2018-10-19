baebe78430d5cbabf7db80fb44f188ab8308458e
Aaron Frank <aaron.frank41@gmail.com>
2018-10-19 16:02:41 -0700

    created methods and constructor for stack and queue class

--- Data-Structures/stacksQueues/StackQueue/StackQueue/Classes/Queue.cs
+++ Data-Structures/stacksQueues/StackQueue/StackQueue/Classes/Queue.cs
@@ -6,5 +6,31 @@ namespace StackQueue.Classes
 {
     class Queue
     {
+        public Node Front { get; set; }
+        
+        public Node Rear { get; set; }
+
+        /// <summary>
+        /// Looks at the Front node in a queue
+        /// </summary>
+        /// <returns>returns the node that was looked at</returns>
+        public Node Peek()
+        {
+            return Front;
+        }
+
+        public void Enqueue(Node node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        public Node Dequeue()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
     }
 }
--- Data-Structures/stacksQueues/StackQueue/StackQueue/Program.cs
+++ Data-Structures/stacksQueues/StackQueue/StackQueue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StackQueue.Classes;
 
 namespace StackQueue
 {
@@ -6,7 +7,29 @@ namespace StackQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node node1 = new Node(10);
+            Node node2 = new Node(20);
+            Node node3 = new Node(30);
+            Node node4 = new Node(40);
+            Node node5 = new Node(50);
+
+            Stack stack = new Stack(node1);
+            stack.Push(node2);
+            stack.Push(node3);
+            stack.Push(node4);
+            stack.Push(node5);
+
+            Console.WriteLine("Nodes in my stack:");
+            Console.WriteLine(stack.Peek().Value);
+            stack.Pop();
+            Console.WriteLine(stack.Peek().Value);
+            stack.Pop();
+            Console.WriteLine(stack.Peek().Value);
+            stack.Pop();
+            Console.WriteLine(stack.Peek().Value);
+            stack.Pop();
+            Console.WriteLine(stack.Peek().Value);
+            stack.Pop();
         }
     }
 }
