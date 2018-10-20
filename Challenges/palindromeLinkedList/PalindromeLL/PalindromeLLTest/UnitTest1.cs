2363c7b96705bdeea571c9d36659c995a531d420
Aaron Frank <aaron.frank41@gmail.com>
2018-10-20 12:42:53 -0700

    forgot to commit. working palindrome check

---
+++ Challenges/palindromeLinkedList/PalindromeLL/PalindromeLLTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+using LinkedLists.Classes;
+using static PalindromeLL.Program;
+
+namespace PalindromeLLTest
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Checks to see if an even lengthed LL is a palindrome
+        /// </summary>
+        [Fact]
+        public void CanTestIfListLengthIsEven()
+        {
+
+        }
+
+        /// <summary>
+        /// Checks to see if an odd lengthed LL is a palindrome
+        /// </summary>
+        [Fact]
+        public void CanTestIfListLengthIsOdd()
+        {
+
+        }
+        
+        /// <summary>
+        /// Checks to see if a LL is not a palindrome
+        /// </summary>
+        [Fact]
+        public void ReturnsFalseIfLLIsNotPalindrome()
+        {
+
+        }       
+    }
+}
