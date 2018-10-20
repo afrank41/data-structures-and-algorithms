7742a63300cfca9f87fc66466fcdb879182f8232
Aaron Frank <aaron.frank41@gmail.com>
2018-10-20 11:59:21 -0700

    tested if list gets reversed

--- Challenges/reverseLinkedList/ReverseLinkedList/ReverseLinkedListTest/UnitTest1.cs
+++ Challenges/reverseLinkedList/ReverseLinkedList/ReverseLinkedListTest/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using LinkedLists.Classes;
+using static ReverseLinkedList.Program;
 
 namespace ReverseLinkedListTest
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// Checks to see if an even lengthed linked list is a palindrome
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void CheckIfListIsReversed()
         {
+            Node node1 = new Node("A");
+            Node node2 = new Node("B");
+            Node node3 = new Node("C");
+            Node node4 = new Node("D");
+            LinkdList list = new LinkdList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.Append(node4);
 
+            Assert.Equal(node4, ReverseLL(list).Head);
         }
     }
 }
