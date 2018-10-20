ebd1bb8803edfd9d6cc7756af53fb6b284f94fda
Aaron Frank <aaron.frank41@gmail.com>
2018-10-20 13:16:02 -0700

    tested if palindrome is odd lengthed

--- Challenges/palindromeLinkedList/PalindromeLL/PalindromeLLTest/UnitTest1.cs
+++ Challenges/palindromeLinkedList/PalindromeLL/PalindromeLLTest/UnitTest1.cs
@@ -9,7 +9,25 @@ namespace PalindromeLLTest
     {
         /// <summary>
         /// Checks to see if an even lengthed LL is a palindrome
-        /// Fun Fact: Aibophobia is the fear of palindromes
+        /// </summary>
+        [Fact]
+        public void CanTestIfListLengthIsEven()
+        {
+            Node node1 = new Node("A");
+            Node node2 = new Node("N");
+            Node node3 = new Node("N");
+            Node node4 = new Node("A");
+            LinkdList list = new LinkdList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.Append(node4);
+
+            Assert.True(PalindromeCheck(list));
+        }
+
+        /// <summary>
+        /// Checks to see if an odd lengthed LL is a palindrome
+        /// Fun Fact: Aibohphobia is the fear of palindromes
         /// </summary>
         [Fact]
         public void CanTestIfListLengthIsOdd()
@@ -18,12 +36,13 @@ namespace PalindromeLLTest
             Node node2 = new Node("I");
             Node node3 = new Node("B");
             Node node4 = new Node("O");
-            Node node5 = new Node("P");
-            Node node6 = new Node("H");
-            Node node7 = new Node("O");
-            Node node8 = new Node("B");
-            Node node9 = new Node("I");
-            Node node10 = new Node("A");
+            Node node5 = new Node("H");
+            Node node6 = new Node("P");
+            Node node7 = new Node("H");
+            Node node8 = new Node("O");
+            Node node9 = new Node("B");
+            Node node10 = new Node("I");
+            Node node11 = new Node("A");
             LinkdList list = new LinkdList(node1);
             list.Append(node2);
             list.Append(node3);
@@ -34,30 +53,7 @@ namespace PalindromeLLTest
             list.Append(node8);
             list.Append(node9);
             list.Append(node10);
-
-            Assert.True(PalindromeCheck(list));
-        }
-
-        /// <summary>
-        /// Checks to see if an odd lengthed LL is a palindrome
-        /// </summary>
-        [Fact]
-        public void CanTestIfListLengthIsEven()
-        {
-            Node node1 = new Node("T");
-            Node node2 = new Node("A");
-            Node node3 = new Node("C");
-            Node node4 = new Node("O");
-            Node node5 = new Node("C");
-            Node node6 = new Node("A");
-            Node node7 = new Node("T");
-            LinkdList list = new LinkdList(node1);
-            list.Append(node2);
-            list.Append(node3);
-            list.Append(node4);
-            list.Append(node5);
-            list.Append(node6);
-            list.Append(node7);
+            list.Append(node11);
 
             Assert.True(PalindromeCheck(list));
         }
