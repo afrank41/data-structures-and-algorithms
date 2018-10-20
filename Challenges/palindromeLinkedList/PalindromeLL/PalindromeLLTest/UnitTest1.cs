f82d0275b7e3d66dbb6a96bf35cefd424e18e96a
Aaron Frank <aaron.frank41@gmail.com>
2018-10-20 12:49:59 -0700

    tested for even palindrome

--- Challenges/palindromeLinkedList/PalindromeLL/PalindromeLLTest/UnitTest1.cs
+++ Challenges/palindromeLinkedList/PalindromeLL/PalindromeLLTest/UnitTest1.cs
@@ -9,22 +9,59 @@ namespace PalindromeLLTest
     {
         /// <summary>
         /// Checks to see if an even lengthed LL is a palindrome
+        /// Fun Fact: Aibophobia is the fear of palindromes
         /// </summary>
         [Fact]
-        public void CanTestIfListLengthIsEven()
+        public void CanTestIfListLengthIsOdd()
         {
+            Node node1 = new Node("A");
+            Node node2 = new Node("I");
+            Node node3 = new Node("B");
+            Node node4 = new Node("O");
+            Node node5 = new Node("P");
+            Node node6 = new Node("H");
+            Node node7 = new Node("O");
+            Node node8 = new Node("B");
+            Node node9 = new Node("I");
+            Node node10 = new Node("A");
+            LinkdList list = new LinkdList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.Append(node4);
+            list.Append(node5);
+            list.Append(node6);
+            list.Append(node7);
+            list.Append(node8);
+            list.Append(node9);
+            list.Append(node10);
 
+            Assert.True(PalindromeCheck(list));
         }
 
         /// <summary>
         /// Checks to see if an odd lengthed LL is a palindrome
         /// </summary>
         [Fact]
-        public void CanTestIfListLengthIsOdd()
+        public void CanTestIfListLengthIsEven()
         {
+            Node node1 = new Node("T");
+            Node node2 = new Node("A");
+            Node node3 = new Node("C");
+            Node node4 = new Node("O");
+            Node node5 = new Node("C");
+            Node node6 = new Node("A");
+            Node node7 = new Node("T");
+            LinkdList list = new LinkdList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.Append(node4);
+            list.Append(node5);
+            list.Append(node6);
+            list.Append(node7);
 
+            Assert.True(PalindromeCheck(list));
         }
-        
+
         /// <summary>
         /// Checks to see if a LL is not a palindrome
         /// </summary>
