981ca7b93343efe22fd1b706145974a62e55e8c7
Aaron Frank <aaron.frank41@gmail.com>
2018-10-20 13:19:51 -0700

    added comments

--- Challenges/palindromeLinkedList/PalindromeLL/PalindromeLL/Program.cs
+++ Challenges/palindromeLinkedList/PalindromeLL/PalindromeLL/Program.cs
@@ -7,54 +7,39 @@ namespace PalindromeLL
     {
         public static void Main(string[] args)
         {
-            //Node node1 = new Node("A");
-            //Node node2 = new Node("N");
-            //Node node3 = new Node("N");
-            //Node node4 = new Node("A");
-            //LinkdList list1 = new LinkdList(node1);
-            //list1.Append(node2);
-            //list1.Append(node3);
-            //list1.Append(node4);
+            Node node1 = new Node("A");
+            Node node2 = new Node("N");
+            Node node3 = new Node("N");
+            Node node4 = new Node("A");
+            LinkdList list1 = new LinkdList(node1);
+            list1.Append(node2);
+            list1.Append(node3);
+            list1.Append(node4);
 
 
-            //Console.WriteLine("First Linked List Test:");
-            //list1.Print();
-            //Console.WriteLine($"\nIs Palindrome: {PalindromeCheck(list1)}");
+            Console.WriteLine("First Linked List Test:");
+            list1.Print();
+            Console.WriteLine($"\nIs Palindrome: {PalindromeCheck(list1)}");
 
-            //Node node5 = new Node("A");
-            //Node node6 = new Node("B");
-            //Node node7 = new Node("C");
-            //Node node8 = new Node("D");
-            //LinkdList list2 = new LinkdList(node5);
-            //list2.Append(node6);
-            //list2.Append(node7);
-            //list2.Append(node8);
+            Node node5 = new Node("A");
+            Node node6 = new Node("B");
+            Node node7 = new Node("C");
+            Node node8 = new Node("D");
+            LinkdList list2 = new LinkdList(node5);
+            list2.Append(node6);
+            list2.Append(node7);
+            list2.Append(node8);
 
-            //Console.WriteLine("\nSecond Linked List Test:");
-            //list2.Print();
-            //Console.WriteLine($"\nIs Palindrome: {PalindromeCheck(list2)}");
-
-            Node node1 = new Node("T");
-            Node node2 = new Node("A");
-            Node node3 = new Node("C");
-            Node node4 = new Node("O");
-            Node node5 = new Node("C");
-            Node node6 = new Node("A");
-            Node node7 = new Node("T");
-            LinkdList list = new LinkdList(node1);
-            list.Append(node2);
-            list.Append(node3);
-            list.Append(node4);
-            list.Append(node5);
-            list.Append(node6);
-            list.Append(node7);
-
-            Console.WriteLine("\nLinked List Test:");
-            list.Print();
-            Console.WriteLine($"\nIs Palindrome: {PalindromeCheck(list)}");
-
+            Console.WriteLine("\nSecond Linked List Test:");
+            list2.Print();
+            Console.WriteLine($"\nIs Palindrome: {PalindromeCheck(list2)}");
         }
 
+        /// <summary>
+        /// Checks to see if a linked list is a palindrome
+        /// </summary>
+        /// <param name="list">linked list to be checked</param>
+        /// <returns>returns true or false whether or not linked list is palindrome</returns>
         public static bool PalindromeCheck(LinkdList list)
         {
             list.Current = list.Head;
