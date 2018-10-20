5fd84917cd77f640747a4fc14a52389d44eaf4b6
Aaron Frank <aaron.frank41@gmail.com>
2018-10-20 11:57:53 -0700

    reverse linked list method works

--- Challenges/reverseLinkedList/ReverseLinkedList/ReverseLinkedList/Program.cs
+++ Challenges/reverseLinkedList/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using LinkedLists.Classes;
 
 namespace ReverseLinkedList
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node node1 = new Node("A");
+            Node node2 = new Node("B");
+            Node node3 = new Node("C");
+            Node node4 = new Node("D");
+            LinkdList list = new LinkdList(node1);
+            list.Append(node2);
+            list.Append(node3);
+            list.Append(node4);
+
+            ReverseLL(list).Print();
+        }
+
+        public static LinkdList ReverseLL(LinkdList list)
+        {
+            Node prev = null;
+            Node curr = list.Head;
+            Node next = curr.Next;
+
+            if (curr == null)
+            {
+                return list;
+            }
+
+            while (curr != null)
+            {
+                next = curr.Next;
+                curr.Next = prev;
+                prev = curr;
+                curr = next;
+            }
+            list.Head = prev;
+
+            return list;
         }
     }
 }
