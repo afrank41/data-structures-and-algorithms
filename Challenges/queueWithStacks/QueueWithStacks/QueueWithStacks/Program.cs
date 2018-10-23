bc5c49b3b5a2d5473aec67f175e63a4aefddbb2b
Aaron Frank <aaron.frank41@gmail.com>
2018-10-23 09:46:13 -0700

    added console output

--- Challenges/queueWithStacks/QueueWithStacks/QueueWithStacks/Program.cs
+++ Challenges/queueWithStacks/QueueWithStacks/QueueWithStacks/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using QueueWithStacks.Classes;
+using StackQueue.Classes;
 
 namespace QueueWithStacks
 {
@@ -6,6 +8,28 @@ namespace QueueWithStacks
     {
         static void Main(string[] args)
         {
+            Stack stack1 = new Stack(null);
+            Stack stack2 = new Stack(null);
+            Cue queue = new Cue(stack1, stack2);
+
+            Node node1 = new Node(10);
+            Node node2 = new Node(20);
+            Node node3 = new Node(30);
+            Node node4 = new Node(40);
+            Node node5 = new Node(50);
+
+            queue.Enqueue(node1);
+            queue.Enqueue(node2);
+            queue.Enqueue(node3);
+            queue.Enqueue(node4);
+            queue.Enqueue(node5);
+
+            Console.WriteLine("Order of queue:");
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
         }
     }
 }
