c345a19ad625f4bcdfe95b52e9d648a586441a92
Aaron Frank <aaron.frank41@gmail.com>
2018-10-27 11:53:23 -0700

    node class props and ctor created

--- Data-Structures/tree/Tree/Tree/Classes/Node.cs
+++ Data-Structures/tree/Tree/Tree/Classes/Node.cs
@@ -6,5 +6,15 @@ namespace Tree.Classes
 {
     public class Node
     {
+        public int Value { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
     }
 }
