3b681303ed17a9c744a0c55bb4aa620091b6efe0
Aaron Frank <aaron.frank41@gmail.com>
2018-10-31 15:04:40 -0700

    wrote breadth first search method

---
+++ Challenges/breadthFirst/BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace BreadthFirstTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node6 = new Node(6, null, null);
+            Node node5 = new Node(5, null, null);
+            Node node4 = new Node(4, null, null);
+            Node node3 = new Node(3, null, null);
+            Node node2 = new Node(2, node5, node6);
+            Node node1 = new Node(1, node3, node4);
+            Node root = new Node(0, node1, node2);
+
+            BreadthFirst(root);
+        }
+
+        static void BreadthFirst(Node root)
+        {
+            Queue<Node> queue = new Queue<Node>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node front = queue.Dequeue();
+                Console.WriteLine(front.Value);
+
+                if (front.Left != null)
+                {
+                    queue.Enqueue(front.Left);
+                }
+
+                if (front.Right != null)
+                {
+                    queue.Enqueue(front.Right);
+                }
+            }
+        }
+    }
+}
