6644fd4ac461f4644f07daa95ffbe46a6da8f157
Aaron Frank <aaron.frank41@gmail.com>
2018-11-02 09:03:23 -0700

    added max value method

---
+++ Challenges/maxValueTree/MaxValueTree/MaxValueTree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Tree.Classes;
+
+namespace MaxValueTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node6 = new Node(6, null, null);
+            Node node5 = new Node(5, null, null);
+            Node node4 = new Node(10, null, null);
+            Node node3 = new Node(3, null, null);
+            Node node2 = new Node(2, node5, node6);
+            Node node1 = new Node(1, node3, node4);
+            Node root = new Node(0, node1, node2);
+            BinaryTree tree = new BinaryTree(root);
+
+            Console.WriteLine("Max Value in Tree:");
+            Console.WriteLine(MaxValue(tree));
+        }
+
+        static int MaxValue(BinaryTree tree)
+        {
+            int max = 0;
+
+            foreach(Node item in tree.PreOrder(tree.Root))
+            {
+                if(item.Value > max)
+                {
+                    max = item.Value;
+                }
+
+            }
+
+            return max;
+        }
+    }
+}
