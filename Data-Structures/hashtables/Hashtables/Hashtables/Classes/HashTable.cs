fb2ca922f217e26d345bdeaea0a370feec6db43b
Aaron Frank <aaron.frank41@gmail.com>
2018-11-12 16:22:22 -0800

    wrote get hash method

--- Data-Structures/hashtables/Hashtables/Hashtables/Classes/HashTable.cs
+++ Data-Structures/hashtables/Hashtables/Hashtables/Classes/HashTable.cs
@@ -8,5 +8,19 @@ namespace Hashtables.Classes
     class HashTable
     {
         LinkdList[] table = new LinkdList[1024];
+
+        public int GetHash(string key)
+        {
+            int hashIndex = 0;
+            foreach (char letter in key)
+            {
+                hashIndex += (int)letter;
+            }
+
+            hashIndex *= 601;
+            hashIndex %= 1024;
+
+            return hashIndex;
+        }
     }
 }
