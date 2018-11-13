37527648ef78a9c5ea4b539a7a200f323fe49f37
Aaron Frank <aaron.frank41@gmail.com>
2018-11-12 19:58:18 -0800

    filled out requirements in main method

--- Data-Structures/hashtables/Hashtables/Hashtables/Program.cs
+++ Data-Structures/hashtables/Hashtables/Hashtables/Program.cs
@@ -12,8 +12,15 @@ namespace Hashtables
             table.Add("dog", "dog");
             table.Add("bird", "bird");
 
-            Console.WriteLine(table.Contains("dog"));
-            Console.WriteLine(table.Contains("puppy"));
+            Console.WriteLine($"Added {table.Find("cat")}, {table.Find("dog")}, and {table.Find("bird")} to the table\n");
+
+            Console.WriteLine($"Forcing a collision at index {table.GetHash("dog")} in the hash table\n");
+            table.Add("dog", "puppy");
+            Console.WriteLine($"Two keys with values \"dog\" and \"puppy\" collided at index {table.GetHash("dog")}\n");
+
+            Console.WriteLine($"Hashtable contains \"cat\": {table.Contains("cat")}");
+            Console.WriteLine($"Hashtable contains \"dog\": {table.Contains("dog")}");
+            Console.WriteLine($"Hashtable contains \"bird\": {table.Contains("bird")}");
         }
     }
 }
