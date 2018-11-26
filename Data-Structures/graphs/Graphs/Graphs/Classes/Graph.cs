cf6e03ce6b8335c6e304f8f087bba82761c4eee8
Aaron Frank <aaron.frank41@gmail.com>
2018-11-26 15:52:55 -0800

    started implementation

---
+++ Data-Structures/graphs/Graphs/Graphs/Classes/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classes
+{
+    class Graph
+    {
+        public LinkedList<LinkedList<Tuple<Node, int>>> AdjacenyList { get; set; }
+
+        public Graph()
+        {
+            AdjacenyList = new LinkedList<LinkedList<Tuple<Node, int>>>();
+        }
+
+        public void AddEdge(Node parent, Tuple<Node, int> child)
+        {
+            foreach (var item in AdjacenyList)
+            {
+                var node = item.First.Value.Item1;
+                var tuple = item.First.Value.Item2;
+            }
+        }
+
+        public void AddEdge(Node parent)
+        {
+
+        }
+    }
+}
